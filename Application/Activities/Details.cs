67918e69606c61076d49bd5f1b6a8c0ef9a7e869
GorlovAndrej <avgorlov899@gmail.com>
2024-04-12 14:36:07 +0300

    add UserFollowing

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -1,7 +1,7 @@
 using Application.Core;
+using Application.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
-using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -19,17 +19,19 @@ namespace Application.Activities
         {
             private readonly DataContext _context;
             public IMapper _mapper { get; }
+            private readonly IUserAccessor _userAccessor;
 
-            public Handler(DataContext context, IMapper mapper)
+            public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
             {
                 _mapper = mapper;
                 _context = context;
+                _userAccessor = userAccessor;
             }
 
             public async Task<Result<ActivityDto>> Handle(Query request, CancellationToken cancellationToken)
             {
                  var activity = await _context.Activities
-                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
+                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider, new{currentUsername = _userAccessor.GetUsername()})
                     .FirstOrDefaultAsync(x => x.Id == request.Id);
                 return Result<ActivityDto>.Success(activity);
             }
--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,4 +1,5 @@
 using Application.Core;
+using Application.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using MediatR;
@@ -17,18 +18,21 @@ namespace Application.Activities
             private readonly DataContext _context;
             public ILogger<List> _logger { get; }
             public IMapper _mapper { get; }
+            private readonly IUserAccessor _userAccessor;
 
-            public Handler(DataContext context, IMapper mapper) 
+            public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor) 
             {
                 _mapper = mapper;
                 _context= context;
+                _userAccessor = userAccessor;
             }
           
             public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
                 var activities = await _context.Activities
-                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
-                    .ToListAsync(cancellationToken);
+                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider,
+                        new {currentUsername = _userAccessor.GetUsername()})
+                        .ToListAsync(cancellationToken);
 
                 return Result<List<ActivityDto>>.Success(activities);
             }
