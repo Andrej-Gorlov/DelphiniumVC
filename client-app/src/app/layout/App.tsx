bc794f7c963e398da68b6e07a7dd59dc9cf6ce2c
GorlovAndrej <avgorlov899@gmail.com>
2024-04-19 11:33:38 +0300

    preparing for production

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -2,7 +2,7 @@ import { Container } from "semantic-ui-react";
 // import List from "semantic-ui-react/dist/commonjs/elements/List";
 import NavBar from "./NavBar";
 import { observer } from "mobx-react-lite";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, ScrollRestoration, useLocation } from "react-router-dom";
 import HomePage from "../../features/home/HomePage";
 import { ToastContainer } from "react-toastify";
 import { useStore } from "../stores/store";
@@ -26,6 +26,7 @@ function App() {
 
   return (
     <>
+    <ScrollRestoration />
     <ModalContainer />
     <ToastContainer position="bottom-right" hideProgressBar theme="colored"/>
     {location.pathname === '/' ? <HomePage/> : (
--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -1,28 +1,29 @@
 import { Navigate, RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../layout/App";
-import HomePage from "../../features/home/HomePage";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
-import TestError from "../../features/errors/TestErrors";
 import NotFound from "../../features/errors/NotFound";
 import ServerError from "../../features/errors/ServerError";
 import TestErrors from "../../features/errors/TestErrors";
 import LoginForm from "../../features/users/LoginForm";
 import ProfilePage from "../../features/profiles/ProfilePage";
+import RequireAuth from "./RequireAuth";
 
 export const routes: RouteObject[] = [
   {
     path: "/",
     element: <App />,
     children: [
-      { path: "activities", element: <ActivityDashboard /> },
-      { path: "activities/:id", element: <ActivityDetails /> },
-      { path: "createActivity", element: <ActivityForm key="create" /> },
-      { path: "manage/:id", element: <ActivityForm key="manage" /> },
-      { path: "profiles/:username", element: <ProfilePage /> },
-      { path: "login", element: <LoginForm /> },
-      { path: "errors", element: <TestErrors /> },
+      { element: <RequireAuth />, children:[
+        { path: "activities", element: <ActivityDashboard /> },
+        { path: "activities/:id", element: <ActivityDetails /> },
+        { path: "createActivity", element: <ActivityForm key="create" /> },
+        { path: "manage/:id", element: <ActivityForm key="manage" /> },
+        { path: "profiles/:username", element: <ProfilePage /> },
+        //{ path: "login", element: <LoginForm /> },
+        { path: "errors", element: <TestErrors /> },
+      ]},
       { path: "not-found", element: <NotFound /> },
       { path: "server-error", element: <ServerError /> },
       { path: "*", element: <Navigate replace to="/not-found" /> },
